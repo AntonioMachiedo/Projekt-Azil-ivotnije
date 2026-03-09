2a16edc0d3947e8f6330e805daf396992d494c20
AntonioMachiedo <antonio.machiedo@skole.hr>
2026-03-09 13:44:32 +0100

    dodana admin klasa

--- Projekt-AzilŽivotnije/Admin.cs
+++ Projekt-AzilŽivotnije/Admin.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,25 @@ namespace Projekt_AzilŽivotnije
 {
     public static class Admin
     {
+        public static void SaveAnimals(string zapis)
+        {
+            using (StreamWriter sw = new StreamWriter("Životinje.txt", true))
+            {
+                sw.WriteLine(zapis);
+            }
+        }
+        public static List<string> GetAllAsStrings()
+        {
+            List<string> lista = new List<string>();
+            using (StreamReader sr = new StreamReader("Životinje.txt"))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    lista.Add(line);
+                }
+            }
+            return lista;
+        }
     }
 }
--- Projekt-AzilŽivotnije/Životinja.cs
+++ Projekt-AzilŽivotnije/Životinja.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
 
 namespace Projekt_AzilŽivotnije
 {
@@ -14,6 +16,17 @@ namespace Projekt_AzilŽivotnije
         public string vrsta { get; set; }
         public string pasmina { get; set; }
         public bool spol { get; set; }
+        public int dob { get; set; }
+        public string datumDolaska { get; set; }
+        public string napomena { get; set; }
+        public bool cijepljen { get; set; }
+        public bool kastriran { get; set; }
 
+    
+
+    public override string ToString()
+        {
+            return ime + ";" + vrsta + ";" + pasmina + ";" + spol + ";" + dob + ";" + datumDolaska + ";" + napomena + ";" + cijepljen + ";" + kastriran;
+        }
     }
 }
